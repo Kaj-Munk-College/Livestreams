d86a9ab8dec8a84b8f1059a9c5de6b37189c33f8
minestarnl <bramkoene@gmail.com>
2020-11-11 22:56:35 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -18,34 +18,6 @@ export default {
 
   mounted() {},
 
-  methods: {
-    addToCalendar: function () {
-      this.loading = true;
-      const event = {
-        start: [2020, 11, 13, 19, 30],
-        duration: { hours: 1, minutes: 0 },
-        title: "Kleinkunstavond",
-        description: "Bekijk de kleinkunstavond",
-        location: "Livestream",
-        url: "https://kleinkunstlive.kajmunk.nl",
-        categories: ["Kaj Munk College", "kleinkunstavond"],
-        status: "CONFIRMED",
-        busyStatus: "BUSY",
-        organizer: { name: "Kaj munkcollege", email: "av@kajmunk.nl" },
-      };
-
-      ics.createEvent(event, (error, value) => {
-        if (error) {
-          console.log(error);
-          return;
-        }
-
-        var data = new Blob([value], { type: "text/x-vCalendar" });
-
-        saveAs(data, "kleinkunstavond.ics");
-        this.loading = false;
-      });
-    },
-  },
+  methods: {},
 };
 </script>
